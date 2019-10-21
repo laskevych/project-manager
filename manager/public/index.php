2cf64cf77880463a7f29cc114883c9833541b911
Andrew Laskevych <an.laskevych@gmail.com>
2019-10-21 12:37:53 +0300

    init docker

---
+++ manager/public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+echo '<html lang="en">
+    <head>
+        <title>Example</title>
+    </head>
+    <body>
+        <h1>This is an example of a simple HTML page with one paragraph.</h1>
+    </body>
+</html>';
