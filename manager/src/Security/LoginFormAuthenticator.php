0b49e6ce9f83079b61928e079df312b32457c974
Andrew Laskevych <an.laskevych@gmail.com>
2019-10-23 14:48:09 +0300

    Revert "Created registration using symfony-make-bundle, updated access_control"
    This reverts commit 7d260b92

--- manager/src/Security/LoginFormAuthenticator.php
+++ manager/src/Security/LoginFormAuthenticator.php
@@ -86,10 +86,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        #throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-
-        return new RedirectResponse($this->urlGenerator->generate('home'));
-
+        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
