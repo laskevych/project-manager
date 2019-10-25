159a6bee72ee9da6f5268a5ed9ea8e998ab09372
Andrew Laskevych <an.laskevych@gmail.com>
2019-10-25 12:51:14 +0300

    Added User Entity

---
+++ manager/src/Model/User/Entity/User/User.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\Entity\User;
+
+
+class User
+{
+    private const STATUS_WAIT = 'wait';
+    private const STATUS_ACTIVE = 'active';
+
+    /**
+     * @var Id
+     */
+    private $id;
+
+    /**
+     * @var \DateTimeImmutable
+     */
+    private $date;
+
+    /**
+     * @var Email
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $passwordHash;
+
+    /**
+     * @var string|null
+     */
+    private $confirmToken;
+
+    /**
+     * @var string
+     */
+    private $status;
+
+    public function __construct(Id $id, \DateTimeImmutable $date, Email $email, string $hash, string $token)
+    {
+        $this->id = $id;
+        $this->date = $date;
+        $this->email = $email;
+        $this->passwordHash = $hash;
+        $this->confirmToken = $token;
+        $this->status = self::STATUS_WAIT;
+    }
+
+    public function isWait(): bool
+    {
+        return $this->status === self::STATUS_WAIT;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->status === self::STATUS_ACTIVE;
+    }
+
+    /**
+     * @return Id
+     */
+    public function getId(): Id
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return \DateTimeImmutable
+     */
+    public function getDate(): \DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    public function getEmail(): Email
+    {
+        return $this->email;
+    }
+
+    public function getPasswordHash(): string
+    {
+        return $this->passwordHash;
+    }
+
+    /**
+     * @return string
+     */
+    public function getConfirmToken(): string
+    {
+        return $this->confirmToken;
+    }
+}
---
+++ manager/src/Model/User/UseCase/SignUp/Request/Handler.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\UseCase\SignUp\Request;
+
+use App\Model\User\Entity\User\Email;
+use App\Model\User\Entity\User\Id;
+use App\Model\User\Entity\User\User;
+use App\Model\User\Service\PasswordHasher;
+use App\Model\User\Entity\User\UserRepository;
+use App\Model\User\Flusher;
+use App\Model\User\Service\ConfirmTokenizer;
+use App\Model\User\Service\ConfirmTokenSender;
+
+class Handler
+{
+    private $users;
+    private $hasher;
+    private $flusher;
+    private $tokenizer;
+    public $sender;
+
+    public function __construct(
+        UserRepository $users,
+        PasswordHasher $hasher,
+        ConfirmTokenizer $tokenizer,
+        ConfirmTokenSender $sender,
+        Flusher $flusher
+    )
+    {
+        $this->users = $users;
+        $this->hasher = $hasher;
+        $this->tokenizer = $tokenizer;
+        $this->sender = $sender;
+        $this->flusher = $flusher;
+    }
+
+    public function handle(Command $command): void
+    {
+        $email = new Email($command->email);
+
+        if ($this->users->hasByEmail($email)) {
+            throw new \DomainException('User already exists.');
+        }
+
+        $user = new User(
+            Id::next(),
+            new \DateTimeImmutable(),
+            $email,
+            $this->hasher->hash($command->password),
+            $token = $this->tokenizer->generate()
+        );
+
+        $this->users->add($user);
+
+        $this->sender->send($email, $token);
+
+        $this->flusher->flush();
+    }
+}
